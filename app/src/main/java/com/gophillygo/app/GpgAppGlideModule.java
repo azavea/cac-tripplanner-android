4b9e258b0613af178baf4e3858aa435fc89e98c7
Kathryn Killebrew <kkillebrew@azavea.com>
2018-02-12 16:48:07 -0500

    Appease Glide
    Silence log warning by adding AppGlideModule, which does not override any default.

---
+++ app/src/main/java/com/gophillygo/app/GpgAppGlideModule.java
@@ -0,0 +1,13 @@
+package com.gophillygo.app;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Glide expects this module to be defined, even if it overrides nothing.
+ * See: http://bumptech.github.io/glide/doc/configuration.html
+ */
+
+@GlideModule
+public class GpgAppGlideModule extends AppGlideModule {
+}
