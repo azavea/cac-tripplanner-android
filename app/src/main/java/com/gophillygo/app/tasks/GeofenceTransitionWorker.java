dec4dbef9517c4e9d23c355b3236c2b1a042dc72
Kathryn Killebrew <kkillebrew@azavea.com>
2018-06-03 20:49:03 -0400

    Send geofence notifications
    Send simple notification on geofence entry or exit.

--- app/src/main/java/com/gophillygo/app/tasks/GeofenceTransitionWorker.java
+++ app/src/main/java/com/gophillygo/app/tasks/GeofenceTransitionWorker.java
@@ -1,14 +1,21 @@
 package com.gophillygo.app.tasks;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
+import com.gophillygo.app.R;
 
 import androidx.work.Data;
 import androidx.work.Worker;
-import dagger.android.AndroidInjection;
 
 public class GeofenceTransitionWorker extends Worker {
 
@@ -16,6 +23,8 @@ public class GeofenceTransitionWorker extends Worker {
     public static final String ERROR_CODE_KEY = "error_code";
     public static final String TRANSITION_KEY = "transition";
     public static final String TRIGGERING_GEOFENCES = "triggering_geofences";
+
+    private static final String CHANNEL_ID = "gophillygo-nearby-places";
 
     private static final String LOG_LABEL = "GeofenceTransition";
 
@@ -81,14 +90,46 @@ public class GeofenceTransitionWorker extends Worker {
         String[] geofences = data.getStringArray(TRIGGERING_GEOFENCES);
 
         if (geofences.length > 0) {
+            Handler handler = new Handler(Looper.getMainLooper());
             for (String geofenceID : geofences) {
                 // TODO: send notification
                 if (enteredGeofence) {
                     Log.d(LOG_LABEL, "Entered geofence ID " + geofenceID);
+
+                    // FIXME: set up channel
+                    // https://developer.android.com/training/notify-user/build-notification
+
+                    handler.post(() -> {
+                        createNotificationChannel();
+                        // show on UI thread
+                        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                                .setSmallIcon(R.drawable.watershed_alliance_full_icon_300x104px)
+                                .setContentTitle("Near a place")
+                                .setContentText("Something interesting is nearby")
+                                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+                        // notificationId is a unique int for each notification that you must define
+                        notificationManager.notify(Integer.valueOf(geofenceID), mBuilder.build());
+                    });
+
                 } else {
                     Log.d(LOG_LABEL, "Exited geofence ID " + geofenceID);
                     // TODO: remove and re-register geofence, or else it will ignore future events
+                    createNotificationChannel();
+                    handler.post(() -> {
+                        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                                .setSmallIcon(R.drawable.watershed_alliance_full_icon_300x104px)
+                                .setContentTitle("Left a place")
+                                .setContentText("Passed by something interesting nearby")
+                                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+                        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+                        // notificationId is a unique int for each notification that you must define
+                        notificationManager.notify(Integer.valueOf(geofenceID), mBuilder.build());
+                    });
                 }
 
             }
@@ -99,4 +140,21 @@ public class GeofenceTransitionWorker extends Worker {
             return WorkerResult.SUCCESS;
         }
     }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = CHANNEL_ID;
+            String description = getApplicationContext().getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getApplicationContext().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
