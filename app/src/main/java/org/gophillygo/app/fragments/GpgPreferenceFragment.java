b6b6d5ebca47ce454f2c268b5b25cd241c334887
Kathryn Killebrew <kkillebrew@azavea.com>
2018-07-06 14:25:03 -0400

    Remove horizontal rules from preferences
    Do not show the default dividers between list items on the settings screen.

--- app/src/main/java/org/gophillygo/app/fragments/GpgPreferenceFragment.java
+++ app/src/main/java/org/gophillygo/app/fragments/GpgPreferenceFragment.java
@@ -3,6 +3,8 @@ package org.gophillygo.app.fragments;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ListView;
 
 import org.gophillygo.app.R;
 
@@ -12,5 +14,15 @@ public class GpgPreferenceFragment extends PreferenceFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // remove dividers: https://stackoverflow.com/a/27952333
+        View rootView = getView();
+        ListView list = rootView.findViewById(android.R.id.list);
+        list.setDivider(null);
     }
 }
