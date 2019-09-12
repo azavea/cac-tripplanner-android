931fcde054484fde707ca519b4a7ae6a14bca6ce
Kathryn Killebrew <kkillebrew@azavea.com>
2019-09-12 16:28:30 -0400

    Use MaterialButtonToggleBar in filter modal
    Fixes #168.
    
    Fix crash after androidx upgrade on opening filter modal.
    Switch to using MaterialButtons in a MaterialButtonToggleBar.
    Does not allow layout to fill horizontal width.

--- app/src/main/java/org/gophillygo/app/GpgToggleButton.java
+++ app/src/main/java/org/gophillygo/app/GpgToggleButton.java
@@ -1,15 +1,19 @@
 package org.gophillygo.app;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.Gravity;
-import android.widget.CompoundButton;
+
+import androidx.databinding.BindingAdapter;
+
+import com.google.android.material.button.MaterialButton;
 
 /**
  * Custom toggle button that changes colors with state.
  */
 
-public class GpgToggleButton extends CompoundButton {
+public class GpgToggleButton extends MaterialButton {
 
     public GpgToggleButton(Context context) {
         this(context, null);
@@ -17,8 +21,17 @@ public class GpgToggleButton extends CompoundButton {
 
     public GpgToggleButton(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
-        setBackgroundResource(R.drawable.toggle_button_selector);
+
+        setBackgroundTintList(getResources()
+                .getColorStateList(R.color.toggle_button_background_color_selector));
+
         setGravity(Gravity.CENTER);
         setClickable(true);
+    }
+
+    @BindingAdapter("app:onCheckedChanged")
+    public static void setOnCheckedChanged(MaterialButton button, OnCheckedChangeListener listener) {
+        button.clearOnCheckedChangeListeners();
+        button.addOnCheckedChangeListener(listener);
     }
 }
