bf1b34a16c83fec5b89082d44e34dd9919977a20
Kathryn Killebrew <kkillebrew@azavea.com>
2019-09-13 18:13:51 -0400

    Revert to CompoundButton
    Revert from MaterialButton to CompoundButton to manage state.
    Handle missing check change listener by adding handler.

--- app/src/main/java/org/gophillygo/app/GpgToggleButton.java
+++ app/src/main/java/org/gophillygo/app/GpgToggleButton.java
@@ -1,9 +1,10 @@
 package org.gophillygo.app;
 
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
+import android.widget.CompoundButton;
 
 import androidx.databinding.BindingAdapter;
 
@@ -13,7 +14,9 @@ import com.google.android.material.button.MaterialButton;
  * Custom toggle button that changes colors with state.
  */
 
-public class GpgToggleButton extends MaterialButton {
+public class GpgToggleButton extends CompoundButton {
+
+    private static final String LOG_LABEL = "GpgToggleButton";
 
     public GpgToggleButton(Context context) {
         this(context, null);
@@ -21,17 +24,15 @@ public class GpgToggleButton extends MaterialButton {
 
     public GpgToggleButton(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
-
-        setBackgroundTintList(getResources()
-                .getColorStateList(R.color.toggle_button_background_color_selector));
-
+        setBackgroundResource(R.drawable.toggle_button_selector);
         setGravity(Gravity.CENTER);
         setClickable(true);
     }
 
+
     @BindingAdapter("app:onCheckedChanged")
-    public static void setOnCheckedChanged(MaterialButton button, OnCheckedChangeListener listener) {
-        button.clearOnCheckedChangeListeners();
-        button.addOnCheckedChangeListener(listener);
+    public static void setOnCheckedChanged(CompoundButton button, OnCheckedChangeListener listener) {
+        Log.d(LOG_LABEL, "setOnCheckedChanged is setting the listener");
+        button.setOnCheckedChangeListener(listener);
     }
 }
