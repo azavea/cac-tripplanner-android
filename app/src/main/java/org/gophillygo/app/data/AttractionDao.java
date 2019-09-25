9228768d2715c5e6ae30b21a04f460cd2732c47f
Kathryn Killebrew <kkillebrew@azavea.com>
2019-09-25 11:32:38 -0400

    Fix search autocomplete icons
    Fix icons for event or place in search dropdown.
    Fixes #171.

--- app/src/main/java/org/gophillygo/app/data/AttractionDao.java
+++ app/src/main/java/org/gophillygo/app/data/AttractionDao.java
@@ -9,6 +9,8 @@ import androidx.room.Query;
 import androidx.room.Transaction;
 import androidx.room.Update;
 
+import org.gophillygo.app.R;
+
 import java.util.List;
 
 
@@ -19,13 +21,15 @@ import java.util.List;
 interface AttractionDao<T> {
 
     @Query("SELECT destination._id AS _id, destination.name AS suggest_text_1, " +
-            "'android.resource://org.gophillygo.app/2131165334' AS suggest_icon_1, " +
+            "'android.resource://org.gophillygo.app/" + R.drawable.ic_place_black_24dp +
+            "' AS suggest_icon_1, " +
             "0 AS suggest_intent_data " +
             "FROM destination " +
             "WHERE destination.name LIKE :search " +
             "UNION " +
             "SELECT event._id AS _id, event.name AS suggest_text_1, " +
-            "'android.resource://org.gophillygo.app/2131165314' AS suggest_icon_1, " +
+            "'android.resource://org.gophillygo.app/" + R.drawable.ic_event_black_24dp +
+            "' AS suggest_icon_1, " +
             "1 AS suggest_intent_data " +
             "FROM event " +
             "WHERE event.name LIKE :search ")
